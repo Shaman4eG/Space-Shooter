f405b7c9749bfbee831188a3dfb9fb84dfb0be70
Daniel <danielghazi00@gmail.com>
2017-09-08 12:20:10 +0300

    Initial filling + VS for git ignore

---
+++ Space Shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+    private Transform tf;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        tf = GetComponent<Transform>();
+
+        rb.velocity = tf.forward * speed;
+    }
+}
